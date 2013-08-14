9e8507bc575af9b292c7c107136164c43670cb7f
Keith Gadberry <me@kdude63.com>
2013-08-13 17:08:18 -0700

    Updated to Bukkit 1.6.2, fixed logging. (How tired was I when I first implemented that?)

--- src/com/kdude63/antisplosion/main.java
+++ src/com/kdude63/antisplosion/main.java
@@ -1,18 +1,20 @@
 package com.kdude63.antisplosion;
 
-import java.io.File;
-//import java.util.List;
-import java.util.logging.Logger;
-
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+//import java.util.List;
+
 public class main extends JavaPlugin implements Listener
 {
-	Logger log = Logger.getLogger("Minecraft");
+	Logger logger = Logger.getLogger("Minecraft");
 	FileConfiguration config;
 	
 	Boolean enderdragon;
@@ -28,6 +30,7 @@ public class main extends JavaPlugin implements Listener
 		config = getConfig();
         if (!new File(this.getDataFolder().getPath() + File.separatorChar + "config.yml").exists())
             saveDefaultConfig();
+
         enderdragon = config.getBoolean("enderdragon");
         wither = config.getBoolean("wither");
         creeper = config.getBoolean("creeper");
@@ -66,15 +69,18 @@ public class main extends JavaPlugin implements Listener
 		}
 		
 		if (logging) {
-			// Have to put these here since I can't perform toString().substring() on a primitive.
-			Double x = e.getLocation().getX();
-			Double y = e.getLocation().getY();
-			Double z = e.getLocation().getZ();
-			
+			Double xd = e.getLocation().getX();
+			Double yd = e.getLocation().getY();
+			Double zd = e.getLocation().getZ();
+
+            Integer x = xd.intValue();
+            Integer y = yd.intValue();
+            Integer z = zd.intValue();
+
 			// Log the explosion and it's world/position to the console.
-			System.out.println("AntiSplosion stopped an explosion in world '" + e.getLocation().getWorld().getName() + "'");
-			System.out.println("Coordinates of cancelled explosion are X|" + x.toString().substring(0, 2) + " Y|" + y.toString().substring(0, 2) + " Z|" + z.toString().substring(0, 2));
-			System.out.println("Explosion was caused by " + e.getEntityType());
+			logger.log(Level.INFO, "AntiSplosion stopped an explosion in world '" + e.getLocation().getWorld().getName() + "'");
+            logger.log(Level.INFO, "Coordinates of cancelled explosion are X|" + x + " Y|" + y + " Z|" + z);
+            logger.log(Level.INFO, "Explosion was caused by " + e.getEntityType());
 		}
 	}
 }
