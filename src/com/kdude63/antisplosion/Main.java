22f52f263da3be69281423c9bca68000705c8099
kdude63 <me@kdude63.com>
2013-11-09 15:00:28 -0800

    Fixed formatting again.

--- src/com/kdude63/antisplosion/Main.java
+++ src/com/kdude63/antisplosion/Main.java
@@ -15,83 +15,83 @@ import java.util.logging.Logger;
 //import java.util.List;
 
 public class Main extends JavaPlugin implements Listener {
-    Logger logger = Logger.getLogger("Minecraft");
-    FileConfiguration config;
+	Logger logger = Logger.getLogger("Minecraft");
+	FileConfiguration config;
 
-    Boolean enderdragon;
-    Boolean wither;
-    Boolean creeper;
-    Boolean tnt;
-    Boolean tntcart;
-    Boolean ghast;
-    Boolean logging;
+	Boolean enderdragon;
+	Boolean wither;
+	Boolean creeper;
+	Boolean tnt;
+	Boolean tntcart;
+	Boolean ghast;
+	Boolean logging;
 
-    public void onEnable()	{
+	public void onEnable()	{
 
-        getServer().getPluginManager().registerEvents(this, this);
-        config = getConfig();
-        if (!new File(this.getDataFolder().getPath() + File.separatorChar + "config.yml").exists())
-            saveDefaultConfig();
+		getServer().getPluginManager().registerEvents(this, this);
+		config = getConfig();
+		if (!new File(this.getDataFolder().getPath() + File.separatorChar + "config.yml").exists())
+			saveDefaultConfig();
 
-        enderdragon = config.getBoolean("enderdragon");
-        wither = config.getBoolean("wither");
-        creeper = config.getBoolean("creeper");
-        tnt = config.getBoolean("tnt");
-        tntcart = config.getBoolean("tntcart");
-        ghast = config.getBoolean("ghast");
-        logging = config.getBoolean("logging");
+		enderdragon = config.getBoolean("enderdragon");
+		wither = config.getBoolean("wither");
+		creeper = config.getBoolean("creeper");
+		tnt = config.getBoolean("tnt");
+		tntcart = config.getBoolean("tntcart");
+		ghast = config.getBoolean("ghast");
+		logging = config.getBoolean("logging");
 
-        try {
-            MetricsLite metrics = new MetricsLite(this);
-            metrics.start();
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, e.getMessage());
-        }
+		try {
+			MetricsLite metrics = new MetricsLite(this);
+			metrics.start();
+		} catch (IOException e) {
+			logger.log(Level.SEVERE, e.getMessage());
+		}
 
-    }
+	}
 
-    @EventHandler
-    public void onEntityExplode(EntityExplodeEvent e) {
-        // Cancels the explosion based on configuration parameters. Still damages entities, but not blocks.
+	@EventHandler
+	public void onEntityExplode(EntityExplodeEvent e) {
+		// Cancels the explosion based on configuration parameters. Still damages entities, but not blocks.
 
-        switch(e.getEntityType()) {
-            default:
-                break;
-            case PRIMED_TNT:
-                if (tnt)
-                    e.setCancelled(true);
-                break;
-            case MINECART_TNT:
-                if (tntcart)
-                    e.setCancelled(true);
-                break;
-            case FIREBALL:
-                if (ghast)
-                    e.setCancelled(true);
-                break;
-            case WITHER_SKULL:
-                if (wither)
-                    e.setCancelled(true);
-                break;
-            case CREEPER:
-                if (creeper)
-                    e.setCancelled(true);
-                break;
-            case ENDER_DRAGON:
-                if (enderdragon)
-                    e.setCancelled(true);
-        }
+		switch(e.getEntityType()) {
+			default:
+				break;
+			case PRIMED_TNT:
+				if (tnt)
+					e.setCancelled(true);
+				break;
+			case MINECART_TNT:
+				if (tntcart)
+					e.setCancelled(true);
+				break;
+			case FIREBALL:
+				if (ghast)
+					e.setCancelled(true);
+				break;
+			case WITHER_SKULL:
+				if (wither)
+					e.setCancelled(true);
+				break;	
+			case CREEPER:
+				if (creeper)
+					e.setCancelled(true);
+				break;
+			case ENDER_DRAGON:
+				if (enderdragon)
+					e.setCancelled(true);
+		}
 
-        if (logging) {
+		if (logging) {
 
-            Integer x = (int)e.getLocation().getX();
-            Integer y = (int)e.getLocation().getY();
-            Integer z = (int)e.getLocation().getZ();
+			Integer x = (int)e.getLocation().getX();
+			Integer y = (int)e.getLocation().getY();
+			Integer z = (int)e.getLocation().getZ();
 
-            // Log the explosion and it's world/position to the console.
-            logger.log(Level.INFO, "AntiSplosion stopped an explosion in world '" + e.getLocation().getWorld().getName() + "'");
-            logger.log(Level.INFO, "Coordinates of cancelled explosion are X:" + x + " Y:" + y + " Z:" + z);
-            logger.log(Level.INFO, "Explosion was caused by " + e.getEntityType());
-        }
-    }
+			// Log the explosion and it's world/position to the console.
+			logger.log(Level.INFO, "AntiSplosion stopped an explosion in world '" + e.getLocation().getWorld().getName() + "'");
+			logger.log(Level.INFO, "Coordinates of cancelled explosion are X:" + x + " Y:" + y + " Z:" + z);
+			logger.log(Level.INFO, "Explosion was caused by " + e.getEntityType());
+		}
+	}
 }
